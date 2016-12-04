1e44fdf03cad8656f6b6d46dc8ef2a599e3d6566
0virax0 <filippovis@outlook.it>
2016-12-04 15:13:34 +0100

    2modified .bat example

--- bin/start_silently.bat
+++ bin/start_silently.bat
@@ -1,3 +1,2 @@
-cd \path_to\packt-claim-bot\bin
-start /min BOTpkt.exe /l login@email.com /p mypassw /d y /s
+start /min \path_to\packt-claim-bot\bin\BOTpkt.exe /l login@email.com /p mypassw /d y /s
 
